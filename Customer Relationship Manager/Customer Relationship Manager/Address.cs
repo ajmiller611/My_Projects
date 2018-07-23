2baa5fb93d2085c9d679a5f5590eaa713149bf1a
Andrew Miller <ajmiller611@live.com>
2018-07-22 23:36:46 -0400

    CRM program work in progress

---
+++ Customer Relationship Manager/Customer Relationship Manager/Address.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer_Relationship_Manager
+{
+    class Address
+    {
+        public string Street { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+        public string ZipCode { get; private set; }
+
+        public Address(string street, string city, string state, string zipCode)
+        {
+            Street = street;
+            City = city;
+            State = state;
+            ZipCode = zipCode;            
+        }
+
+        public override string ToString()
+        {
+            return Street + "/n" + City + ", " + State + " " + ZipCode;
+        }
+    }
+}
---
+++ Customer Relationship Manager/Customer Relationship Manager/Client.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer_Relationship_Manager
+{
+    class Client
+    {
+        //TODO create a client buying history class or database
+        public string ClientName { get; private set; }
+        public string CompanyName { get; private set; }
+        public string PhoneNumber { get; private set; }
+        public string Notes { get; private set; }        
+        private Address address;
+
+        public Client(string clientName, string companyName, string phoneNumber, string notes, Address address)
+        {
+            ClientName = clientName;
+            CompanyName = companyName;
+            PhoneNumber = phoneNumber;
+            Notes = notes;
+            this.address = address;
+        }
+
+        public Client(string clientName, string phoneNumber, string notes, Address address)
+        {
+            ClientName = clientName;            
+            PhoneNumber = phoneNumber;
+            Notes = notes;
+            this.address = address;
+        }
+    }
+}
